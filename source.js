630827072873d73448efa325e93c6be07bdf2f68
Decy Rynolds <decymayela80@gmail.com>
2026-03-01 21:44:54 -0500

    fixed truncate

--- source.js
+++ source.js
@@ -9,7 +9,7 @@
  let btnclose= document.querySelector("#btnclose")
  
  btnclose.addEventListener("click",()=>{
-    console.log("add clicked!")
+    
 
     overlay.classList.add("hidden")
 
@@ -35,6 +35,12 @@ function add(){
    
     let raw=document.getElementById("input").value 
 
+    let dateLine= document.getElementById("date").value
+
+    let deadlineValue = dateLine===""?null: new Date(dateLine).getTime()
+
+    new Date(dateLine).getDate()
+
     //remove white space from the input
     const input_user= raw.trim()
    
@@ -51,7 +57,8 @@ function add(){
       id:nextID++,
       task_name:input_user,
       deadline:null,
-      complete:false
+      complete:false,
+      deadline: deadlineValue 
     }
 
     //test for duplicate task 
@@ -74,6 +81,7 @@ function add(){
      //screanRender();
     
     render()
+    document.getElementById("date").value="";
 }
 
 // create a render to build all the view UI
@@ -87,10 +95,14 @@ function render(){
     let li=document.createElement("li");
     li.dataset.id=task.id
     li.classList.toggle("done",task.complete);
+
+    let textContent=document.createElement("div")
+        textContent.className = "text-content"
     
     let span = document.createElement("span");
     span.className="task-text"
     span.textContent=task.task_name
+    
 
     let btnDelete= document.createElement("button")
     btnDelete.className="btnDelete"
@@ -105,20 +117,34 @@ function render(){
     let div = document.createElement("div");
     div.className="task-action"
     div.append(checkbox);
-    div.append(btnDelete)
-
+    div.append(btnDelete);
+    let small = document.createElement("small")
+    
+    
    // li.append(checkbox)
-    li.append(span)
+   // li.append(span)
+    textContent.append(span)
+    if (task.deadline) textContent.append(small)
+      small.textContent = new Date(task.deadline).toLocaleString()
+      
+       
+    li.append(textContent)
     li.append(div)
     tasklist.appendChild(li)
 
-    requestAnimationFrame(()=>screanRender(span))
+requestAnimationFrame(()=>{
+  const isTruncate = span.scrollWidth > span.clientWidth
+  span.classList.toggle("is-truncate" , isTruncate)
+
+    })
+
+   
 
   })
 
 
 }
-
+/*
 function screanRender(span){
 
   if(span.scrollWidth> span.clientWidth)
@@ -135,6 +161,7 @@ function screanRender(span){
       }
 
 }
+      */
 
 function deletion(){
   
@@ -168,4 +195,32 @@ document.getElementById("task-list").addEventListener("change",(e)=>{
   task.complete = e.target.checked
   li.classList.toggle("done",task.complete)
    
+})
+
+document.getElementById("task-list").addEventListener("click",(e)=>{
+  let span = e.target.closest(".task-text")
+  if(!span)return
+
+  //only open the model if test if truncate
+  const isTruncate = span.scrollWidth > span.clientWidth;
+  if(!isTruncate)return
+
+  //finds the li that span belong to
+  let li = span.closest("li")
+  if(!li)return
+
+  //find the task id 
+  let id = Number(li.dataset.id)
+
+  //find the task in array
+
+  let task = Todo.find(t=>t.id===id)
+
+  if(!task)return;
+  //display the full text
+  fullText.textContent= task.task_name
+
+  overlay.classList.remove("hidden")
+  console.log("task clickes overflow")
+
 })
